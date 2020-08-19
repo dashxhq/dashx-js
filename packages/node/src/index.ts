4ebc4449bf182edd8fd25e5ed4b0420334937e16
Rajat Sharma <lunasunkaiser@gmail.com>
2020-08-19 18:40:38 +0530

    Add dashx-node (#7)
    * Add dashx-node
    
    * Add build:node in scripts

---
+++ packages/node/src/index.ts
@@ -0,0 +1,11 @@
+import Client from './Client'
+
+type Params = {
+  baseUri: string,
+  publicKey: string,
+  privateKey: string
+}
+
+export default {
+  createClient: (params: Params): Client => new Client(params)
+}
