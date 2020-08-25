983578a7106aa6e72da021c250b5f8d5c1a7d090
Rajat Sharma <lunasunkaiser@gmail.com>
2020-08-25 10:36:15 +0530

    WIP: TrackRequest

--- packages/react-native/android/src/main/java/com/dashx/sdk/DashXData.kt
+++ packages/react-native/android/src/main/java/com/dashx/sdk/DashXData.kt
@@ -3,8 +3,9 @@ package com.dashx.sdk
 import com.google.gson.JsonObject
 
 data class TrackRequest(
+    val uid: String?,
     val event: String,
-    val data: JsonObject
+    val data: JsonObject?
 )
 
 data class GenericResponse(
