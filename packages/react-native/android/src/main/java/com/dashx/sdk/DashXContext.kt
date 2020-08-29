904a16e6f7e8dc56dd4dd21025367491f9f6f542
Rajat Sharma <lunasunkaiser@gmail.com>
2020-08-29 11:36:31 +0530

    Get DisplayMetrics, Locale and Timezone

--- packages/react-native/android/src/main/java/com/dashx/sdk/DashXContext.kt
+++ packages/react-native/android/src/main/java/com/dashx/sdk/DashXContext.kt
@@ -1,7 +1,40 @@
 package com.dashx.sdk
 
-class DashXContext {
+import android.content.Context
+import android.os.Build
+import com.facebook.react.bridge.ReactApplicationContext
+import java.util.*
+
+class DashXContext(reactApplicationContext: ReactApplicationContext) {
+    private var displayMetrics: HashMap<String, String?>
+    private var locale: String
+    private val timezone: String
+
+    private fun getDisplayMetrics(context: Context): HashMap<String, String?> {
+        val displayMetrics = context.resources.displayMetrics
+        return hashMapOf(
+            "height" to displayMetrics.heightPixels.toString(),
+            "width" to displayMetrics.widthPixels.toString(),
+            "density" to displayMetrics.density.toString()
+        )
+    }
+
+    private fun getLocale(context: Context): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            context.resources.configuration.locales[0].language
+        } else {
+            context.resources.configuration.locale.language
+        }
+    }
+
+    private fun getTimezone(): String {
+        return TimeZone.getDefault().displayName.toString()
+    }
+
     init {
-        
+        val context = reactApplicationContext.applicationContext
+        locale = getLocale(context)
+        displayMetrics = getDisplayMetrics(context)
+        timezone = getTimezone()
     }
 }
