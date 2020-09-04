baad56b1ee0264008b2f72f91b7d713d1fafa522
Rajat Sharma <lunasunkaiser@gmail.com>
2020-09-04 20:54:56 +0530

    Revert packages/react-native/index.js

--- packages/react-native/index.js
+++ packages/react-native/index.js
@@ -2,14 +2,18 @@ import { NativeModules } from 'react-native';
 
 const { DashX } = NativeModules;
 
-const nativeIdentify = DashX.identify;
+const { identify, track } = DashX;
 
-DashX.identify = (uid, options) => {
-    if (typeof uid === "string") {
-        return nativeIdentify(uid, options || null)
-    } else {
-        return nativeIdentify(null, uid) // options are passed as first parameter
-    }
-}
+// Handle overloads at JS, because Native modules doesn't allow that
+// https://github.com/facebook/react-native/issues/19116
+DashX.identify = (options) => {
+  if (typeof options === 'string') {
+    return identify(options, null); // options is a string ie. uid
+  } else {
+    return identify(null, options);
+  }
+};
+
+DashX.track = (event, data) => track(event, data || null);
 
 export default DashX;
