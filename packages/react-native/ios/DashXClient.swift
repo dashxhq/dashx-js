b091031af00ed3c736cd1a85cac525aa5347096f
Rajat Sharma <lunasunkaiser@gmail.com>
2020-09-05 12:05:33 +0530

    integer -> string

--- packages/react-native/ios/DashXClient.swift
+++ packages/react-native/ios/DashXClient.swift
@@ -3,15 +3,15 @@ import Foundation
 struct DashXClient {
     static let instance = DashXClient()
     private var anonymousUid
-    
+
     private init() { }
-    
+
     private func generateAnonymousUid() {
         let preferences = UserDefaults.standard
         let anonymousUidKey = "DashX.anonymousUid"
-        
+
         if (preferences.object(forKey: anonymousUidKey) != nil) {
-            self.anonymousUid = preferences.integer(forKey: anonymousUidKey)
+            self.anonymousUid = preferences.string(forKey: anonymousUidKey)
         } else {
             self.anonymousUid = UUID().uuidString
             preferences.set(self.anonymousUid, forKey: anonymousUidKey)
