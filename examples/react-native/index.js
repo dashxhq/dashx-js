f3ed5b4965fbd95ed52760dc06158461d5b25ce5
Rajat Sharma <lunasunkaiser@gmail.com>
2020-09-07 20:30:58 +0530

    Fix/remove public key (#17)
    * Use react-native-config to access public key
    
    * Insert newline at EOF
    
    * Add editorconfig rules for swift

--- examples/react-native/index.js
+++ examples/react-native/index.js
@@ -2,19 +2,20 @@
  * @format
  */
 
+import Config from 'react-native-config';
+import DashX from '@dashx/react-native';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import DashX from '@dashx/react-native';
 
 DashX.setLogLevel(0);
 
 DashX.setup({
-  publicKey: 'ceaTX7hJTevAKRiMe61gwZpW',
+  publicKey: Config.DASHX_PUBLIC_KEY,
   baseUri: 'http://api.dashx-staging.com/v1',
 });
 
-DashX.identify('uid');
+DashX.identify({ firstName: 'John', lastName: 'Doe' });
 DashX.track('Hello World');
 
 AppRegistry.registerComponent(appName, () => App);
