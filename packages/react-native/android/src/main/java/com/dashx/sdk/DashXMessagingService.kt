1f96bf14a930f45aba41da25922869a55110d426
Rajat Sharma <lunasunkaiser@gmail.com>
2020-09-10 13:07:55 +0530

    Remove ? from DashXClient

--- packages/react-native/android/src/main/java/com/dashx/sdk/DashXMessagingService.kt
+++ packages/react-native/android/src/main/java/com/dashx/sdk/DashXMessagingService.kt
@@ -5,16 +5,16 @@ import com.google.firebase.messaging.RemoteMessage
 
 class DashXMessagingService : FirebaseMessagingService() {
     private val tag = DashXMessagingService::class.java.simpleName
-    private val dashXClient: DashXClient? = DashXClient.instance
+    private val dashXClient: DashXClient = DashXClient.instance
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        dashXClient?.setDeviceToken(token)
+        dashXClient.setDeviceToken(token)
         DashXLog.d(tag, "onNewToken: $token")
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
-        dashXClient?.handleMessage(remoteMessage)
+        dashXClient.handleMessage(remoteMessage)
     }
 }
