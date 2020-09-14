3257032f89821f6f96126b085aa75ebf63e87077
Rajat Sharma <lunasunkaiser@gmail.com>
2020-09-14 22:21:39 +0530

    Add from_background flag ios

--- packages/react-native/ios/Constants.swift
+++ packages/react-native/ios/Constants.swift
@@ -3,7 +3,7 @@ import Foundation
 struct Constants {
     static let PACKAGE_NAME = "com.dashx.sdk"
     static let USER_PREFERENCES_KEY_ANONYMOUS_UID = "\(PACKAGE_NAME).anonymous_uid"
-    static let USER_PREFERENCES_KEY_VERSION = "\(PACKAGE_NAME).version"
+    static let USER_PREFERENCES_KEY_BUILD = "\(PACKAGE_NAME).build"
     static let INTERNAL_EVENT_APP_INSTALLED = "Application Installed"
     static let INTERNAL_EVENT_APP_UPDATED = "Application Updated"
     static let INTERNAL_EVENT_APP_OPENED = "Application Opened"
