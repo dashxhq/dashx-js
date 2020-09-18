3577d2130090a62427296347875289ad1d020b6e
Rajat Sharma <lunasunkaiser@gmail.com>
2020-09-18 11:25:36 +0530

    Add DashX notification channel

---
+++ packages/react-native/android/src/main/java/com/dashx/sdk/notificationbuilders/NotificationChannel.kt
@@ -0,0 +1,9 @@
+@file:JvmName("DashXNotificationChannel")
+package com.dashx.sdk.notificationbuilders
+
+object DashXNotificationChannel {
+    @JvmField
+    var id = "com.dashx.sdk.notificationchannel"
+    @JvmField
+    var name = "DashX"
+}
