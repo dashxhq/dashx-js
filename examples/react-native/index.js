0ed495cdc2f0ddcf95bff363a4450561b03ef15f
Rajat Sharma <lunasunkaiser@gmail.com>
2020-09-23 10:27:36 +0530

    Reverted examples/react-native/index.js

--- examples/react-native/index.js
+++ examples/react-native/index.js
@@ -12,7 +12,7 @@ import DashX from '@dashx/react-native';
 DashX.setLogLevel(0);
 
 DashX.setup({
-  publicKey: 'FKSLzpLhn3dL6XixRQEtkLv7',
+  publicKey: Config.DASHX_PUBLIC_KEY,
   baseUri: 'http://api.dashx-staging.com/v1',
   trackAppLifecycleEvents: true,
   trackScreenViews: true
@@ -27,7 +27,5 @@ DashX.track('Added Product to Cart', {
   isFeatured: true,
   coupon: null
 });
-
-DashX.setIdentityToken('');
 
 AppRegistry.registerComponent(appName, () => App);
