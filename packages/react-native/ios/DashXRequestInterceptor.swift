66abf12075ee667896a91f8f5c078fb75d789dab
Rajat Sharma <lunasunkaiser@gmail.com>
2020-10-06 03:18:13 +0530

    Swift: Get retry time delay based on battary status

--- packages/react-native/ios/DashXRequestInterceptor.swift
+++ packages/react-native/ios/DashXRequestInterceptor.swift
@@ -10,11 +10,29 @@ class DashXRequestInterceptor: RequestInterceptor {
         self.retryLimit = retryLimit
     }
     
+    func getTimeDelay(for retryCount: Int) -> TimeInterval {
+        var batteryState: UIDevice.BatteryState { UIDevice.current.batteryState }
+        switch batteryState {
+        case .charging:
+            return pow(Double(exponentialBackoffBase), Double(retryCount)) * exponentialBackoffScale
+        default:
+            return pow(Double(exponentialBackoffBase), Double(retryCount + 1)) * exponentialBackoffScale
+        }
+    }
+    
+    func checkMethod(_ request: Request) -> Bool {
+        if let httpMethod = request.request?.method,
+            httpMethod == .post {
+            return true
+        } else {
+            return false
+        }
+    }
+    
     func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
         if request.retryCount < retryLimit,
-            let httpMethod = request.request?.method,
-            httpMethod == .post {
-            let timeDelay = pow(Double(exponentialBackoffBase), Double(request.retryCount)) * exponentialBackoffScale
+            checkMethod(request) {
+            let timeDelay = getTimeDelay(for: request.retryCount)
             completion(.retryWithDelay(timeDelay))
         } else {
             completion(.doNotRetry)
