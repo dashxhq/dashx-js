ff2f5289e70953d1b15ce499f4b8db6c55e61622
Rajat Sharma <lunasunkaiser@gmail.com>
2020-10-07 07:10:55 +0530

    Swift: Add jitterFactor while retrying

--- packages/react-native/ios/DashXRequestInterceptor.swift
+++ packages/react-native/ios/DashXRequestInterceptor.swift
@@ -2,21 +2,19 @@ import Foundation
 import Alamofire
 
 class DashXRequestInterceptor: RequestInterceptor {
-    private let retryLimit: Int
+    private let retryLimit: Int = 3
     private let exponentialBackoffScale: Double = 0.5
+    private let jitterFactor: Double = 0.1
     private let exponentialBackoffBase: Int = 2
     
-    init(_ retryLimit: Int) {
-        self.retryLimit = retryLimit
-    }
-    
     private func getTimeDelay(for retryCount: Int) -> TimeInterval {
+        let randomFactor: Double = 1 + (1 - Double.random(in: 0..<1) * 2) * jitterFactor
         var batteryState: UIDevice.BatteryState { UIDevice.current.batteryState }
         switch batteryState {
         case .charging:
-            return pow(Double(exponentialBackoffBase), Double(retryCount)) * exponentialBackoffScale
+            return (pow(Double(exponentialBackoffBase), Double(retryCount)) * exponentialBackoffScale) * randomFactor
         default:
-            return pow(Double(exponentialBackoffBase), Double(retryCount + 1)) * exponentialBackoffScale
+            return (pow(Double(exponentialBackoffBase), Double(retryCount + 1)) * exponentialBackoffScale) * randomFactor
         }
     }
     
