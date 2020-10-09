0df010d3f96f305feae511ee27fff500e905fa7e
Rajat Sharma <lunasunkaiser@gmail.com>
2020-10-09 08:53:20 +0530

    Add retrying

--- packages/node/src/HttpClient.ts
+++ packages/node/src/HttpClient.ts
@@ -1,6 +1,7 @@
 import http from 'got'
 import Keyv from 'keyv'
 import os from 'os'
+import uuid from 'uuid-random'
 import type { Response } from 'got'
 
 import { snakeCaseKeys } from './utils'
@@ -15,6 +16,12 @@ class HttpClient {
   private privateKey?: string
 
   private baseUri: string
+
+  private exponentialBackoffBase = 2.0
+
+  private exponentialBackoffScale = 0.5
+
+  private jitterFactor = 0.1
 
   constructor(baseUri: string, publicKey?: string, privateKey?: string) {
     this.getCache = (cacheTimeout: number) => new Keyv(
@@ -34,11 +41,22 @@ class HttpClient {
     return this
   }
 
+  getRetryTimeDelay(retryCount: number): number {
+    const randomFactor = 1 + (1 - Math.random() * 2) * this.jitterFactor
+    return (this.exponentialBackoffBase ** retryCount * this.exponentialBackoffScale) * randomFactor
+  }
+
   makeRequest<T>(uri: string, body: T): Promise<Response> {
     return http(uri, {
-      json: snakeCaseKeys(body),
+      json: snakeCaseKeys({ ...body, request_id: uuid() }),
       method: 'POST',
       prefixUrl: this.baseUri,
+      retry: {
+        limit: 3,
+        methods: [ 'POST' ],
+        calculateDelay: ({ attemptCount }) => this.getRetryTimeDelay(attemptCount),
+        statusCodes: [ 408, 500, 502, 503, 504 ]
+      },
       cache: this.cacheTimeout ? this.getCache(this.cacheTimeout) : false,
       headers: {
         'User-Agent': 'dashx-node',
