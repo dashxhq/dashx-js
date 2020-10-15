ade7cc1b5e366c1d9cb2f772cd3d22b6113d705a
Rajat Sharma <lunasunkaiser@gmail.com>
2020-10-15 16:39:10 +0530

    Add swiftlint

--- packages/react-native/ios/DashXUIViewController.swift
+++ packages/react-native/ios/DashXUIViewController.swift
@@ -6,7 +6,7 @@ extension UIViewController {
         let swizzledSelector = #selector(swizzledViewDidLoad)
         swizzler(UIViewController.self, UIViewController.self, originalSelector, swizzledSelector)
     }
-    
+
     @objc func swizzledViewDidLoad() {
         DashXClient.instance.track(Constants.INTERNAL_EVENT_APP_SCREEN_VIEWED, withData: [:])
         // Call the original viewDidLoad - using the swizzledViewDidLoad signature
--- packages/react-native/ios/Utils.swift
+++ packages/react-native/ios/Utils.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-let swizzler: (AnyClass, AnyClass, Selector, Selector) -> () = { mainClass, swizzledClass, originalSelector, swizzledSelector in
+let swizzler: (AnyClass, AnyClass, Selector, Selector) -> Void = { mainClass, swizzledClass, originalSelector, swizzledSelector in
     guard let swizzledMethod = class_getInstanceMethod(swizzledClass, swizzledSelector) else {
         return
     }
-    
-    if let originalMethod = class_getInstanceMethod(mainClass, originalSelector)  {
+
+    if let originalMethod = class_getInstanceMethod(mainClass, originalSelector) {
         method_exchangeImplementations(originalMethod, swizzledMethod)
     } else {
         class_addMethod(mainClass, swizzledSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
