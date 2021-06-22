fb0a052bcb0f0ddc245022067dcfda3b36234ab7
Rajat Sharma <lunasunkaiser@gmail.com>
2021-06-22 19:07:58 +0530

    Fix deliver

--- packages/node/src/utils.ts
+++ packages/node/src/utils.ts
@@ -1,17 +1,23 @@
+import type { Parcel } from './Client'
+
 export function parseFilterObject(filterObject: Record<string, any> = {}) {
-    const filterBy: Record<string, any> = {};
+  const filterBy: Record<string, any> = {}
 
-    Object.keys(filterObject).forEach(key => {
-        if (key.startsWith("_")) {
-            filterBy[key.substring(1)] = filterObject[key]
-            return
-        }
+  Object.keys(filterObject).forEach((key) => {
+    if (key.startsWith('_')) {
+      filterBy[key.substring(1)] = filterObject[key]
+      return
+    }
 
-        filterBy.data = {
-            [key]: filterObject[key],
-            ...filterBy.data
-        }
-    })
+    filterBy.data = {
+      [key]: filterObject[key],
+      ...filterBy.data
+    }
+  })
 
-    return filterBy
+  return filterBy
 }
+
+export const createParcel = (
+  { to = [], cc = [], bcc = [], data = {}, channel } : Parcel
+) => ({ to: Array.isArray(to) ? to : [ to ], cc, bcc, data, channel: channel.toUpperCase() })
