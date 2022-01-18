8bcfd30a6a6dfede4c24beb5508b3638c1090e00
Afzal Sayed <14029371+afzalsayed96@users.noreply.github.com>
2022-01-18 20:13:45 +0530

    Prevent duplicate subscribe calls (#127)

--- packages/react-native/ios/Constants.swift
+++ packages/react-native/ios/Constants.swift
@@ -3,6 +3,7 @@ import Foundation
 struct Constants {
     static let PACKAGE_NAME = "com.dashx.sdk"
     static let USER_PREFERENCES_KEY_ANONYMOUS_UID = "\(PACKAGE_NAME).anonymous_uid"
+    static let USER_PREFERENCES_KEY_DEVICE_TOKEN = "\(PACKAGE_NAME).device_token"
     static let USER_PREFERENCES_KEY_BUILD = "\(PACKAGE_NAME).build"
     static let INTERNAL_EVENT_APP_INSTALLED = "Application Installed"
     static let INTERNAL_EVENT_APP_UPDATED = "Application Updated"
