bd65a366d132310312f6303b06868ceec0a8b6fc
Danish Shah <shah.danish.1996@gmail.com>
2022-03-15 19:40:06 +0530

    review changes

--- examples/web/src/index.js
+++ examples/web/src/index.js
@@ -23,8 +23,7 @@ dx.searchContent('page', { returnType: 'all', limit: 10 })
 
 dx.fetchContent('page/new').then(console.log);
 dx.fetchStoredPreferences("0111c6f9-e8c8-492e-aa94-678b76bd3dc5").then((res) => {
-  const prefData = res.preferenceData
-  dx.saveStoredPreferences("0111c6f9-e8c8-492e-aa94-678b76bd3dc5", prefData).then(console.log)
+  dx.saveStoredPreferences("0111c6f9-e8c8-492e-aa94-678b76bd3dc5", res).then(console.log)
 })
 
 ReactDOM.render(
