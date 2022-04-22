8f843b2a36971a915a403a9955cc87a23b4e4c1e
Amandeep Singh Ghai <Amandeepsinghghai@users.noreply.github.com>
2022-04-22 15:07:26 +0530

    Ios notifications (#159)

--- packages/react-native/ios/DashXAppDelegate.swift
+++ packages/react-native/ios/DashXAppDelegate.swift
@@ -3,23 +3,28 @@ import Foundation
 @objc(DashXAppDelegate)
 class DashXAppDelegate: NSObject {
     static func swizzleDidReceiveRemoteNotification() {
-        DispatchQueue.main.async {
-            let appDelegate = UIApplication.shared.delegate
-            let appDelegateClass = object_getClass(appDelegate)
+        let appDelegate = UIApplication.shared.delegate
+        let appDelegateClass = object_getClass(appDelegate)
 
-            let originalSelector = #selector(UIApplicationDelegate.application(_:didReceiveRemoteNotification:))
-            let swizzledSelector = #selector(
-                DashXAppDelegate.self.handleMessage(_:didReceiveRemoteNotification:)
-            )
+        let originalSelector = #selector(UIApplicationDelegate.application(_:didReceiveRemoteNotification:fetchCompletionHandler:))
+        let swizzledSelector = #selector(DashXAppDelegate.self.handleMessage(_:didReceiveRemoteNotification:fetchCompletionHandler:))
 
-            try? swizzler(appDelegateClass!, DashXAppDelegate.self, originalSelector, swizzledSelector)
+        guard let swizzledMethod = class_getInstanceMethod(DashXAppDelegate.self, swizzledSelector) else {
+            return
+        }
+
+        if let originalMethod = class_getInstanceMethod(appDelegateClass, originalSelector)  {
+            // exchange implementation
+            method_exchangeImplementations(originalMethod, swizzledMethod)
+        } else {
+            // add implementation
+            class_addMethod(appDelegateClass, swizzledSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
         }
     }
 
+    // Based on - https://firebase.google.com/docs/cloud-messaging/ios/receive
     @objc
-    func handleMessage(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
-        var properties: [String: [String: Any]] = [ "notification": [:] ]
-
+    func handleMessage(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         DashXLog.d(tag: #function, "Received APN: \(userInfo)")
 
         guard let remoteMessage = try? FirebaseRemoteMessage(decoding: userInfo) else {
@@ -27,9 +32,7 @@ class DashXAppDelegate: NSObject {
             return
         }
 
-        properties["notification"]?["title"] = remoteMessage.aps.alert.title
-        properties["notification"]?["body"] = remoteMessage.aps.alert.body
-
-        DashXEventEmitter.instance.dispatch(name: "messageReceived", body: properties)
+        completionHandler(.newData)
+        DashXEventEmitter.instance.dispatch(name: "messageReceived", body: userInfo)
     }
 }
