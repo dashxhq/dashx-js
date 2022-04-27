200dd6151c4d8ea324a8ad4d98d558955664d446
Gulshan Dhingra <gdgulshan.007@gmail.com>
2022-04-27 13:25:37 +0530

    Launch app on notification click

--- packages/react-native/android/src/main/java/com/dashx/rn/sdk/DashXMessagingService.kt
+++ packages/react-native/android/src/main/java/com/dashx/rn/sdk/DashXMessagingService.kt
@@ -5,7 +5,6 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -82,9 +81,7 @@ class DashXMessagingService : FirebaseMessagingService() {
         title: String?,
         body: String?
     ) : Notification {
-        val intent = getNewBaseIntent()
-
-        val pendingIntent = PendingIntent.getActivity(this, 1, intent, FLAG_UPDATE_CURRENT)
+        val pendingIntent = getNewPendingIntent()
 
         val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(getSmallIcon())
@@ -95,6 +92,30 @@ class DashXMessagingService : FirebaseMessagingService() {
             .setAutoCancel(true)
 
         return notificationBuilder.build()
+    }
+
+    private fun getNewPendingIntent(): PendingIntent {
+        val context = getApplicationContext()
+        val pendingIntentFlags = PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+
+        val intent = getNewBaseIntent()
+
+        val launchIntent = context.packageManager.getLaunchIntentForPackage(context.packageName)
+
+        if (launchIntent == null) {
+            return PendingIntent.getActivity(context, 1, intent, pendingIntentFlags)
+        }
+
+        // Mimic launcher behaviour
+        launchIntent.setPackage(null)
+        launchIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED
+
+        return PendingIntent.getActivities(
+            context,
+            1,
+            arrayOf(launchIntent, intent),
+            pendingIntentFlags
+        )
     }
 
     private fun getNewBaseIntent(): Intent {
